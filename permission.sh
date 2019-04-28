bb846f7ff31000d342ae349b54ce19ad535e3ad4
DineshST <dineshkumar1109@gmail.com>
2019-04-28 09:44:01 +0530

    permission

---
+++ permission.sh
@@ -0,0 +1,14 @@
+To check the permission
+ls -l filename
+to change to permission of file
+x - exceute
+r - read
+w - write
+chmod +x filename -provide the excute permission to a file
+chmod -w fn - remove the write permission to a file
+a - allow to everyone
+go - group and others
+u- user/owner
+
+chmod go-xw filename - remove the executable permission to group and other members
+chmod a+x fname - provide permission all the members/group/owner to execute a file
